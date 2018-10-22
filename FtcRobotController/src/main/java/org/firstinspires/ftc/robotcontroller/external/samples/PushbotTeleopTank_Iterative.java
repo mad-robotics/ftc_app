df9df02555e3cf65d6b3fbfe62c5d722a89345f6
SilverMagpie <alexander.markwell@gmail.com>
2018-10-22 08:47:13 -0700

    Uhh... fixedFixed random autos that exist

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotTeleopTank_Iterative.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotTeleopTank_Iterative.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Pushbot: Teleop Tank", group="Pushbot")
+@TeleOp(name="Pushbot: (drive) Wait this works?", group="Pushbot")
 //@Disabled
 public class PushbotTeleopTank_Iterative extends OpMode{
 
