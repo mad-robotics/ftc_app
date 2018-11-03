3336d632b63eb1fab310338c3b0da6c115426e32
SilverMagpie <alexander.markwell@gmail.com>
2018-11-02 18:29:32 -0700

    ?

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotTeleopTank_Iterative.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotTeleopTank_Iterative.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Pushbot: (drive) Wait this works?", group="Pushbot")
+@TeleOp(name="Pushbot: (drive) Meet 0", group="Pushbot")
 //@Disabled
 public class PushbotTeleopTank_Iterative extends OpMode{
 
