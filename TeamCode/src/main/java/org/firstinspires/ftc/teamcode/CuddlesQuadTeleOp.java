0c6da68ab685a350e21a0607f929427956e755a0
SilverMagpie <alexander.markwell@gmail.com>
2018-11-08 09:11:44 -0800

    OK, last change.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CuddlesQuadTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CuddlesQuadTeleOp.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Vroom Vroom, said Patrick", group="Pushbot")
+@TeleOp(name="Drve█code█that█might█work", group="Pushbot")
 //@Disabled
 public class CuddlesQuadTeleOp extends OpMode{
 
